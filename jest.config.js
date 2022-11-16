a4c45f4331b7055938dbc71979767025de10b037
Ashish Kayastha <108121450+askayastha22@users.noreply.github.com>
2022-11-16 15:42:31 -0600

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,9 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 100,
-      lines: 100,
-      statements: 100
+      branches: 25,
+      lines: 50,
+      statements: 50
     }
   }
 }
