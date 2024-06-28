a4f1b0812da8f2bf893fa6a3ff9278becb9e2f99
Nitin Jadhav <3931042+nitinja@users.noreply.github.com>
2024-06-28 13:48:43 -0500

    Added test fixes and a bugfix

--- test/generator-add-web-assets-cf-admin.test.js
+++ test/generator-add-web-assets-cf-admin.test.js
@@ -11,37 +11,40 @@ governing permissions and limitations under the License.
 
 /* eslint-disable jest/expect-expect */ // => use assert
 
-const helpers = require('yeoman-test')
-const assert = require('yeoman-assert')
-const fs = require('fs')
-const path = require('path')
-const cloneDeep = require('lodash.clonedeep')
+const helpers = require("yeoman-test");
+const assert = require("yeoman-assert");
+const fs = require("fs");
+const path = require("path");
+const cloneDeep = require("lodash.clonedeep");
 
-const CFAdminWebAssetsGenerator = require('../src/generator-add-web-assets-cf-admin')
-const Generator = require('yeoman-generator')
+const CFAdminWebAssetsGenerator = require("../src/generator-add-web-assets-cf-admin");
+const Generator = require("yeoman-generator");
 
-const { customExtensionManifest, demoExtensionManifest } = require('./test-manifests')
+const {
+  customExtensionManifest,
+  demoExtensionManifest,
+} = require("./test-manifests");
 
-const extFolder = 'src/aem-cf-console-admin-1'
-const extConfigPath = path.join(extFolder, 'ext.config.yaml')
-const webSrcFolder = path.join(extFolder, 'web-src')
+const extFolder = "src/aem-cf-console-admin-1";
+const extConfigPath = path.join(extFolder, "ext.config.yaml");
+const webSrcFolder = path.join(extFolder, "web-src");
 
 const basicGeneratorOptions = {
-  'web-src-folder': webSrcFolder,
-  'config-path': extConfigPath
-}
+  "web-src-folder": webSrcFolder,
+  "config-path": extConfigPath,
+};
 
-describe('prototype', () => {
-  test('exports a yeoman generator', () => {
-    expect(CFAdminWebAssetsGenerator.prototype).toBeInstanceOf(Generator)
-  })
-})
+describe("prototype", () => {
+  test("exports a yeoman generator", () => {
+    expect(CFAdminWebAssetsGenerator.prototype).toBeInstanceOf(Generator);
+  });
+});
 
 /**
  * Checks that .env has the required environment variables.
  */
-function assertEnvContent (prevContent) {
-  assert.fileContent('.env', prevContent)
+function assertEnvContent(prevContent) {
+  assert.fileContent(".env", prevContent);
 }
 
 /**
@@ -49,26 +52,27 @@ function assertEnvContent (prevContent) {
  *
  * @param {string} extensionManifest an extension manifest
  */
-function assertFiles (extensionManifest) {
+function assertFiles(extensionManifest) {
   // Asert generated web assets files
-  assert.file(`${webSrcFolder}/index.html`)
-  assert.file(`${webSrcFolder}/src/index.css`)
-  assert.file(`${webSrcFolder}/src/index.js`)
-  assert.file(`${webSrcFolder}/src/components/Constants.js`)
-  assert.file(`${webSrcFolder}/src/components/App.js`)
-  assert.file(`${webSrcFolder}/src/components/ExtensionRegistration.js`)
+  assert.file(`${webSrcFolder}/index.html`);
+  assert.file(`${webSrcFolder}/src/index.css`);
+  assert.file(`${webSrcFolder}/src/index.js`);
+  assert.file(`${webSrcFolder}/src/components/Constants.js`);
+  assert.file(`${webSrcFolder}/src/components/App.js`);
+  assert.file(`${webSrcFolder}/src/components/ExtensionRegistration.js`);
+  assert.file(`${webSrcFolder}/src/components/ExtensionRegistration.js`);
 
   // Assert generated modal files
-  const actionBarButtons = extensionManifest.actionBarButtons || []
-  const headerMenuButtons = extensionManifest.headerMenuButtons || []
-  const allCustomButtons = actionBarButtons.concat(headerMenuButtons)
+  const actionBarButtons = extensionManifest.actionBarButtons || [];
+  const headerMenuButtons = extensionManifest.headerMenuButtons || [];
+  const allCustomButtons = actionBarButtons.concat(headerMenuButtons);
 
   allCustomButtons.forEach((button) => {
     if (button.needsModal) {
-      const modalFileName = button.label.replace(/ /g, '') + 'Modal'
-      assert.file(`${webSrcFolder}/src/components/${modalFileName}.js`)
+      const modalFileName = button.label.replace(/ /g, "") + "Modal";
+      assert.file(`${webSrcFolder}/src/components/${modalFileName}.js`);
     }
-  })
+  });
 }
 
 /**
@@ -76,140 +80,150 @@ function assertFiles (extensionManifest) {
  *
  * @param {string} extensionManifest an extension manifest
  */
- function assertCodeContent (extensionManifest) {
+function assertCodeContent(extensionManifest) {
   assert.fileContent(
     `${webSrcFolder}/src/components/Constants.js`,
     `extensionId: '${extensionManifest.id}'`
-  )
+  );
 
   assert.fileContent(
     `${webSrcFolder}/index.html`,
     '<script src="./src/index.js"'
-  )
+  );
 
-  const actionBarButtons = extensionManifest.actionBarButtons || []
-  const headerMenuButtons = extensionManifest.headerMenuButtons || []
-  const allCustomButtons = actionBarButtons.concat(headerMenuButtons)
+  const actionBarButtons = extensionManifest.actionBarButtons || [];
+  const headerMenuButtons = extensionManifest.headerMenuButtons || [];
+  const allCustomButtons = actionBarButtons.concat(headerMenuButtons);
 
   allCustomButtons.forEach((button) => {
     if (button.needsModal) {
-      const modalFileName = button.label.replace(/ /g, '') + 'Modal'
+      const modalFileName = button.label.replace(/ /g, "") + "Modal";
 
       assert.fileContent(
         `${webSrcFolder}/src/components/App.js`,
         `import ${modalFileName} from "./${modalFileName}"`
-      )
+      );
       assert.fileContent(
         `${webSrcFolder}/src/components/App.js`,
-        `element={<${button.label.replace(/ /g, '')}Modal />}`
-      )
+        `element={<${button.label.replace(/ /g, "")}Modal />}`
+      );
       assert.fileContent(
         `${webSrcFolder}/src/components/${modalFileName}.js`,
         `export default function ${modalFileName} ()`
-      )
+      );
     }
-  })
+  });
 
   headerMenuButtons.forEach((button) => {
     if (button.needsModal) {
       assert.fileContent(
         `${webSrcFolder}/src/components/App.js`,
         `exact path="${button.id}-modal"`
-      )
+      );
       assert.fileContent(
         `${webSrcFolder}/src/components/ExtensionRegistration.js`,
         `const modalURL = "/index.html#/${button.id}-modal"`
-      )
+      );
     }
-  })
+  });
 }
 
-describe('run', () => {
-  const prevDotEnv = 'FAKECONTENT'
+describe("run", () => {
+  const prevDotEnv = "FAKECONTENT";
 
-  test('test a generator invocation with custom code generation', async () => {
-    const options = cloneDeep(basicGeneratorOptions)
-    options['extension-manifest'] = customExtensionManifest
+  test("test a generator invocation with custom code generation", async () => {
+    const options = cloneDeep(basicGeneratorOptions);
+    options["extension-manifest"] = customExtensionManifest;
     await helpers
       .run(CFAdminWebAssetsGenerator)
       .withOptions(options)
       .inTmpDir((dir) => {
-        fs.writeFileSync(path.join(dir, '.env'), prevDotEnv)
-      })
+        fs.writeFileSync(path.join(dir, ".env"), prevDotEnv);
+      });
 
-    assertFiles(customExtensionManifest)
+    assertFiles(customExtensionManifest);
     assertDependencies(
       fs,
       {
-        '@adobe/aio-sdk': expect.any(String),
-        '@adobe/exc-app': expect.any(String),
-        '@adobe/react-spectrum': expect.any(String),
-        '@adobe/uix-guest': expect.any(String),
-        '@react-spectrum/list': expect.any(String),
-        '@spectrum-icons/workflow': expect.any(String),
-        'chalk': expect.any(String),
-        'core-js': expect.any(String),
-        'node-fetch': expect.any(String),
-        'node-html-parser': expect.any(String),
-        'react': expect.any(String),
-        'react-dom': expect.any(String),
-        'react-error-boundary': expect.any(String),
-        'react-router-dom': expect.any(String),
-        'regenerator-runtime': expect.any(String),
+        "@adobe/aio-sdk": expect.any(String),
+        "@adobe/exc-app": expect.any(String),
+        "@adobe/react-spectrum": expect.any(String),
+        "@adobe/uix-guest": expect.any(String),
+        "@react-spectrum/list": expect.any(String),
+        "@spectrum-icons/workflow": expect.any(String),
+        chalk: expect.any(String),
+        "core-js": expect.any(String),
+        "node-fetch": expect.any(String),
+        "node-html-parser": expect.any(String),
+        react: expect.any(String),
+        "react-dom": expect.any(String),
+        "react-error-boundary": expect.any(String),
+        "react-router-dom": expect.any(String),
+        "regenerator-runtime": expect.any(String),
+        "ajv": expect.any(String),
+        "js-yaml": expect.any(String),
       },
       {
-        '@babel/core': expect.any(String),
-        '@babel/plugin-transform-react-jsx': expect.any(String),
-        '@babel/polyfill': expect.any(String),
-        '@babel/preset-env': expect.any(String),
-        '@openwhisk/wskdebug': expect.any(String),
-        'jest': expect.any(String)
+        "@babel/core": expect.any(String),
+        "@babel/plugin-transform-react-jsx": expect.any(String),
+        "@babel/polyfill": expect.any(String),
+        "@babel/preset-env": expect.any(String),
+        "@openwhisk/wskdebug": expect.any(String),
+        jest: expect.any(String),
       }
-    )
-    assertEnvContent(prevDotEnv)
-    assertCodeContent(customExtensionManifest)
-  })
+    );
+    assertScripts(fs, {
+      "transform:yaml-to-json": expect.any(String),
+    });
+    assertEnvContent(prevDotEnv);
+    assertCodeContent(customExtensionManifest);
+  });
 
-  test('test a generator invocation with demo code generation', async () => {
-    const options = cloneDeep(basicGeneratorOptions)
-    options['extension-manifest'] = demoExtensionManifest
+  test("test a generator invocation with demo code generation", async () => {
+    const options = cloneDeep(basicGeneratorOptions);
+    options["extension-manifest"] = demoExtensionManifest;
     await helpers
       .run(CFAdminWebAssetsGenerator)
       .withOptions(options)
       .inTmpDir((dir) => {
-        fs.writeFileSync(path.join(dir, '.env'), prevDotEnv)
-      })
+        fs.writeFileSync(path.join(dir, ".env"), prevDotEnv);
+      });
 
-    assertFiles(demoExtensionManifest)
+    assertFiles(demoExtensionManifest);
     assertDependencies(
       fs,
       {
-        '@adobe/aio-sdk': expect.any(String),
-        '@adobe/exc-app': expect.any(String),
-        '@adobe/react-spectrum': expect.any(String),
-        '@adobe/uix-guest': expect.any(String),
-        '@react-spectrum/list': expect.any(String),
-        '@spectrum-icons/workflow': expect.any(String),
-        'chalk': expect.any(String),
-        'core-js': expect.any(String),
-        'node-fetch': expect.any(String),
-        'node-html-parser': expect.any(String),
-        'react': expect.any(String),
-        'react-dom': expect.any(String),
-        'react-error-boundary': expect.any(String),
-        'react-router-dom': expect.any(String),
-        'regenerator-runtime': expect.any(String),
+        "@adobe/aio-sdk": expect.any(String),
+        "@adobe/exc-app": expect.any(String),
+        "@adobe/react-spectrum": expect.any(String),
+        "@adobe/uix-guest": expect.any(String),
+        "@react-spectrum/list": expect.any(String),
+        "@spectrum-icons/workflow": expect.any(String),
+        chalk: expect.any(String),
+        "core-js": expect.any(String),
+        "node-fetch": expect.any(String),
+        "node-html-parser": expect.any(String),
+        react: expect.any(String),
+        "react-dom": expect.any(String),
+        "react-error-boundary": expect.any(String),
+        "react-router-dom": expect.any(String),
+        "regenerator-runtime": expect.any(String),
+        "ajv": expect.any(String),
+        "js-yaml": expect.any(String),
       },
       {
-        '@babel/core': expect.any(String),
-        '@babel/plugin-transform-react-jsx': expect.any(String),
-        '@babel/polyfill': expect.any(String),
-        '@babel/preset-env': expect.any(String),
-        '@openwhisk/wskdebug': expect.any(String),
-        'jest': expect.any(String)
+        "@babel/core": expect.any(String),
+        "@babel/plugin-transform-react-jsx": expect.any(String),
+        "@babel/polyfill": expect.any(String),
+        "@babel/preset-env": expect.any(String),
+        "@openwhisk/wskdebug": expect.any(String),
+        jest: expect.any(String),
       }
-    )
-    assertEnvContent(prevDotEnv)
-    assertCodeContent(demoExtensionManifest)
-  })
-})
+    );
+    assertScripts(fs, {
+      "transform:yaml-to-json": expect.any(String),
+    });
+    assertEnvContent(prevDotEnv);
+    assertCodeContent(demoExtensionManifest);
+  });
+});
